4ed3aba6fdd11639716b2d537b5b4052189cb667
Zekima <christianledaal@gmail.com>
2021-12-09 13:12:36 +0100

    1s apply

---
+++ dist/_nuxt/static/1639051946/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fmap","\u002F","\u002Fapply","\u002Fresources","\u002Fleaderboard"]})
--- pages/apply.vue
+++ pages/apply.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <head>
-  <meta http-equiv="refresh" content="3; URL=https://docs.google.com/forms/d/e/1FAIpQLScCjoK5TNWobw6h-lWT_qHchfiXJFHvUnOKzQS0OzXduPHo3Q/viewform" />
+  <meta http-equiv="refresh" content="1; URL=https://docs.google.com/forms/d/e/1FAIpQLScCjoK5TNWobw6h-lWT_qHchfiXJFHvUnOKzQS0OzXduPHo3Q/viewform" />
 </head>
 <body>
   <p>If you are not redirected in five seconds, <a href="https://docs.google.com/forms/d/e/1FAIpQLScCjoK5TNWobw6h-lWT_qHchfiXJFHvUnOKzQS0OzXduPHo3Q/viewform">click here</a>.</p>
